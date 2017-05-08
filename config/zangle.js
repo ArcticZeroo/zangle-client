b603ce049f0b16ac154a085e7b54fae0fe5ad99d
Spencer <spencer@artix.cc>
2017-05-08 16:43:49 -0400

    Initial commit - stuff!

---
+++ config/zangle.js
@@ -0,0 +1,10 @@
+module.exports = {
+    BASE_URL: 'https://webconnect.bloomfield.org/zangle/StudentPortal/Home/',
+    routes: {
+        SIGNIN: 'Login',
+        STUDENTLIST: 'PortalMainPage',
+        PROFILE_DATA: 'LoadProfileData/',
+        PRINT: 'PrintStudentReport',
+        SET_STUDENT: 'https://webconnect.bloomfield.org/zangle/StudentPortal/StudentBanner/SetStudentBanner/'
+    }
+};
