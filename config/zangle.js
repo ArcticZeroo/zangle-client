d1a35ff9d640bd1cc15cfcd6ce554b0e420e61f0
Spencer <spencer@artix.cc>
2017-05-08 17:52:00 -0400

    add loading of CLASSES !

--- config/zangle.js
+++ config/zangle.js
@@ -6,5 +6,11 @@ module.exports = {
         PROFILE_DATA: 'LoadProfileData/',
         PRINT: 'PrintStudentReport',
         SET_STUDENT: 'https://webconnect.bloomfield.org/zangle/StudentPortal/StudentBanner/SetStudentBanner/'
+    },
+    regex: {
+        PERIOD: /<label for="period" id="lblperiod">Per<\/label>:[\s\S]*<\/b>([0-9])/,
+        CLASS: /<b>(&#xA0;|&nbsp;)+(.+) \(([0-9]+)\)<\/b>/,
+        TEACHER: /<label for="teacher"[\s\S]*[\r\n]+(.+),(.+)/,
+        GRADE: /Grade<\/label>:[\s\S]*<\/b>[ ]*([A-Z])[ ]*(\(([0-9]{1,2}.[0-9]{1,2})%\))?/
     }
 };
